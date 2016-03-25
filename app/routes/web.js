65e4c113929c6c9f77d6139efe8e3fa894989298
camusd <camusd@oregonstate.edu>
2016-03-25 08:47:30 -0700

    tried to fix some indentation issues

--- app/routes/web.js
+++ app/routes/web.js
@@ -7,127 +7,127 @@ module.exports = function(app, conn) {
 	 * a form and POSTS the data. */
 
 	app.post('/add/cpu', function(req, res) {
-	 	conn.query("CALL check_serial_cpu('"+req.body.serial_input+"');",
-	 		function(error, results, fields){
-	 			if(error) {
-	 				throw error;
-	 			}
-	 			if(results[0].length == 0) {
-	 				conn.query("CALL put_cpu('"+req.body.serial_input+"','"
-	 					+req.body.spec_input+"','"+req.body.mm_input+"','"
-	 					+req.body.freq_input+"','"+req.body.step_input+"','"
-	 					+req.body.llc_input+"','"+req.body.cores_input+"','"
-	 					+req.body.codename_input+"','"+req.body.class_input+"','"
-	 					+req.body.external_input+"','"+req.body.arch_input+"','"
-	 					+req.body.notes_input+"');",
-	 				function(error, results, fields){
-	 					if(error) {
-	 						throw error;
-	 					}
-	 				});
-	 			}
-	 		});
-	 	res.statusCode = 302;
-	 	res.setHeader("Location", "/")
-	 	res.end();
+		conn.query("CALL check_serial_cpu('"+req.body.serial_input+"');",
+			function(error, results, fields){
+				if(error) {
+					throw error;
+				}
+				if(results[0].length == 0) {
+					conn.query("CALL put_cpu('"+req.body.serial_input+"','"
+						+req.body.spec_input+"','"+req.body.mm_input+"','"
+						+req.body.freq_input+"','"+req.body.step_input+"','"
+						+req.body.llc_input+"','"+req.body.cores_input+"','"
+						+req.body.codename_input+"','"+req.body.class_input+"','"
+						+req.body.external_input+"','"+req.body.arch_input+"','"
+						+req.body.notes_input+"');",
+					function(error, results, fields){
+						if(error) {
+							throw error;
+						}
+					});
+				}
+			});
+		res.statusCode = 302;
+		res.setHeader("Location", "/")
+		res.end();
 	});
 
 	app.post('/add/ssd', function(req, res) {
-	 	conn.query("CALL check_serial_ssd('"+req.body.serial_input+"');",
-	 		function(error, results, fields){
-	 			if(error) {
-	 				throw error;
-	 			}
-	 			if(results[0].length == 0) {
-	 				conn.query("CALL put_ssd('"+req.body.serial_input+"','"
-	 					+req.body.manufacturer_input+"','"+req.body.model_input+"','"
-	 					+req.body.capacity_input+"','"+req.body.notes_input+"');",
-	 				function(error, results, fields){
-	 					if(error) {
-	 						throw error;
-	 					}
-	 				});
-	 			}
-	 		});
-	 	res.statusCode = 302;
-	 	res.setHeader("Location", "/")
-	 	res.end();
+		conn.query("CALL check_serial_ssd('"+req.body.serial_input+"');",
+			function(error, results, fields){
+				if(error) {
+					throw error;
+				}
+				if(results[0].length == 0) {
+					conn.query("CALL put_ssd('"+req.body.serial_input+"','"
+						+req.body.manufacturer_input+"','"+req.body.model_input+"','"
+						+req.body.capacity_input+"','"+req.body.notes_input+"');",
+					function(error, results, fields){
+						if(error) {
+							throw error;
+						}
+					});
+				}
+			});
+		res.statusCode = 302;
+		res.setHeader("Location", "/")
+		res.end();
 	});
 
 	app.post('/add/memory', function(req, res) {
-	 	conn.query("CALL check_serial_memory('"+req.body.serial_input+"');",
-	 		function(error, results, fields){
-	 			if(error) {
-	 				throw error;
-	 			}
-	 			if(results[0].length == 0) {
-	 				conn.query("CALL put_memory('"+req.body.serial_input+"','"
-	 					+req.body.manufacturer_input+"','"+req.body.physical_size_input+"','"
-	 					+req.body.memory_type_input+"','"+req.body.capacity_input+"','"
-	 					+req.body.speed_input+"','"+req.body.ecc_input+"','"
-	 					+req.body.ranks_input+"','"+req.body.notes_input+"');",
-	 				function(error, results, fields){
-	 					if(error) {
-	 						throw error;
-	 					}
-	 				});
-	 			}
-	 		});
-	 	res.statusCode = 302;
-	 	res.setHeader("Location", "/")
-	 	res.end();
+		conn.query("CALL check_serial_memory('"+req.body.serial_input+"');",
+			function(error, results, fields){
+				if(error) {
+					throw error;
+				}
+				if(results[0].length == 0) {
+					conn.query("CALL put_memory('"+req.body.serial_input+"','"
+						+req.body.manufacturer_input+"','"+req.body.physical_size_input+"','"
+						+req.body.memory_type_input+"','"+req.body.capacity_input+"','"
+						+req.body.speed_input+"','"+req.body.ecc_input+"','"
+						+req.body.ranks_input+"','"+req.body.notes_input+"');",
+					function(error, results, fields){
+						if(error) {
+							throw error;
+						}
+					});
+				}
+			});
+		res.statusCode = 302;
+		res.setHeader("Location", "/")
+		res.end();
 	});
 
 	app.post('/add/flash', function(req, res) {
-	 	conn.query("CALL check_serial_flash_drive('"+req.body.serial_input+"');",
-	 		function(error, results, fields){
-	 			if(error) {
-	 				throw error;
-	 			}
-	 			if(results[0].length == 0) {
-	 				conn.query("CALL put_flash_drive('"+req.body.serial_input+"','"
-	 									  	   +req.body.manufacturer_input+"','"+req.body.capacity_input+"','"
-	 									  	   +req.body.notes_input+"');",
-	 					function(error, results, fields){
-	 						if(error) {
-	 							throw error;
-	 						}
-	 					});
-	 			}
-	 		});
-	 	res.statusCode = 302;
-	 	res.setHeader("Location", "/")
-	 	res.end();
+		conn.query("CALL check_serial_flash_drive('"+req.body.serial_input+"');",
+			function(error, results, fields){
+				if(error) {
+					throw error;
+				}
+				if(results[0].length == 0) {
+					conn.query("CALL put_flash_drive('"+req.body.serial_input+"','"
+													 +req.body.manufacturer_input+"','"+req.body.capacity_input+"','"
+													 +req.body.notes_input+"');",
+						function(error, results, fields){
+							if(error) {
+								throw error;
+							}
+						});
+				}
+			});
+		res.statusCode = 302;
+		res.setHeader("Location", "/")
+		res.end();
 	});
 
 	/* Routes for loading pages in the web interface */
 
 	app.get('/', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/index.html');
+		res.sendFile(rootdir + '/public/web/index.html');
 	});
 
 	app.get('/add', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/add.html');
+		res.sendFile(rootdir + '/public/web/add.html');
 	});
 
 	app.get('/add/cpu', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/add_cpu.html');
+		res.sendFile(rootdir + '/public/web/add_cpu.html');
 	});
 
 	app.get('/add/ssd', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/add_ssd.html');
+		res.sendFile(rootdir + '/public/web/add_ssd.html');
 	});
 
 	app.get('/add/memory', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/add_memory.html');
+		res.sendFile(rootdir + '/public/web/add_memory.html');
 	});
 
 	app.get('/add/flash', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/add_flash_drive.html');
+		res.sendFile(rootdir + '/public/web/add_flash_drive.html');
 	});
 
 	app.get('/settings', enforceLogin, function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/settings.html');
+		res.sendFile(rootdir + '/public/web/settings.html');
 	});
 
 	app.get('/settings/attributes', enforceAdminLogin, function(req, res) {
@@ -142,11 +142,11 @@ module.exports = function(app, conn) {
 
 	 // For testing purposes. Will need to be deleted.
 	app.get('/testsite', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/testsite.html');
+		res.sendFile(rootdir + '/public/web/testsite.html');
 	});
 
 	app.get('/login', function(req, res) {
-	 	res.sendFile(rootdir + '/public/web/login.html');
+		res.sendFile(rootdir + '/public/web/login.html');
 	});
 
 	/* 
@@ -159,22 +159,22 @@ module.exports = function(app, conn) {
 	 */
 	app.use('/login/login', function(req, res, next) {
 		request.post({url: process.env.CRED_ADDR, form: {'username': req.body.username, 'pass': req.body.pass}},
-	 		function(error, response, body) {
-	 			if (error) {
-	 				console.log(error);
-	 			}
+			function(error, response, body) {
+				if (error) {
+					console.log(error);
+				}
 
-	 			// Checking if the user exists in AD.
-	 			if (body !== '') {
-	 				// We have a user in the AD system. Parse out the wwid.
-	 				req.session.wwid = body;
-	 			} else {
-	 				// No wwid found, erase current session and create new session for user.
-	 				req.session.regenerate();
-	 				res.redirect('/login');
-	 			}
-	 			next();
-	 		});
+				// Checking if the user exists in AD.
+				if (body !== '') {
+					// We have a user in the AD system. Parse out the wwid.
+					req.session.wwid = body;
+				} else {
+					// No wwid found, erase current session and create new session for user.
+					req.session.regenerate();
+					res.redirect('/login');
+				}
+				next();
+			});
 	}, function(req,res,next) {
 		// We know at this point we have a wwid, so let's try to get the user from our DB.
 		conn.query('CALL get_user_from_wwid('+req.session.wwid+')', function(error, results, fields) {
@@ -182,16 +182,16 @@ module.exports = function(app, conn) {
 					throw error;
 				}
 				if (results[0].length === 1) {
-	 				req.session.last_name = results[0][0].last_name;
-	 				req.session.first_name = results[0][0].first_name;
-	 				req.session.is_admin = results[0][0].is_admin;
-	 				req.session.save();
+					req.session.last_name = results[0][0].last_name;
+					req.session.first_name = results[0][0].first_name;
+					req.session.is_admin = results[0][0].is_admin;
+					req.session.save();
 				} else {
 					// Got no results
 					res.redirect('/login');
 				}
 				next();
-	 		});
+			});
 	});
 
 	app.post('/login/login', function(req, res) {
--- public/js/table.js
+++ public/js/table.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
 		if (this.id !== 'colBtn') {
 			$(this).html( '<input type="text" style="width: '+(width)+'px;" placeholder="'+title+'" />' );
 			// The 14 represents the additional amount of space for the column sorting
-      // Not sure if the 14 added to the width is neccessary, deleting for now to make the table smaller.
-    }
-  });
+			// Not sure if the 14 added to the width is neccessary, deleting for now to make the table smaller.
+		}
+	});
 
-  $.get('/data/cpu', function(jsonData) {
-  	var cpu_table = $('#cpu_table').DataTable({
+	$.get('/data/cpu', function(jsonData) {
+		var cpu_table = $('#cpu_table').DataTable({
 		"data": jsonData.items,
 		"columns" : [
 			{"data" : "serial_num"},
@@ -29,28 +29,28 @@ $(document).ready(function() {
 			{"defaultContent": "<button class=\"scrap_btn\">Scrap</button>", "visible": false}
 		],
 		"scrollX" 	  : true,
-	  "paging" 		  : true,
+		"paging" 		  : true,
 		"pagingType"  : "simple_numbers",
 		"pageLength"  : 10,
-	  "fixedHeader" : {
-	    	 "header" : true,
-	    	 "footer" : false
-	    }
-	  });
-	  if (jsonData.is_admin === 1) {
-	  	cpu_table.column(-1).visible(true);
-	  }
-	  // Apply the search
-	  $('#filterCols th').each(function (idx){
-	  	var col = cpu_table.column(idx);
-	  	$('input', this).on( 'keyup change', function () {
-	  		col.search( this.value ).draw();
-	  	});
-	  });
+		"fixedHeader" : {
+				 "header" : true,
+				 "footer" : false
+			}
+		});
+		if (jsonData.is_admin === 1) {
+			cpu_table.column(-1).visible(true);
+		}
+		// Apply the search
+		$('#filterCols th').each(function (idx){
+			var col = cpu_table.column(idx);
+			$('input', this).on( 'keyup change', function () {
+				col.search( this.value ).draw();
+			});
+		});
 	});
 
-  $.get('/data/ssd', function(jsonData) {
-  	var ssd_table = $('#ssd_table').DataTable({
+	$.get('/data/ssd', function(jsonData) {
+		var ssd_table = $('#ssd_table').DataTable({
 		"data": jsonData.items,
 		"columns" : [
 			{"data" : "serial_num"},
@@ -61,28 +61,28 @@ $(document).ready(function() {
 			{"defaultContent": "<button class=\"scrap_btn\">Scrap</button>", "visible": false}
 		],
 		"scrollX" 	  : true,
-	  "paging" 		  : true,
+		"paging" 		  : true,
 		"pagingType"  : "simple_numbers",
 		"pageLength"  : 10,
-	  "fixedHeader" : {
-	    	 "header" : true,
-	    	 "footer" : false
-	    }
-	  });
-	  if (jsonData.is_admin === 1) {
-	  	ssd_table.column(-1).visible(true);
-	  }
-	  // Apply the search
-	  $('#filterCols th').each(function (idx){
-	  	var col = ssd_table.column(idx);
-	  	$('input', this).on( 'keyup change', function () {
-	  		col.search( this.value ).draw();
-	  	});
-	  });
+		"fixedHeader" : {
+				 "header" : true,
+				 "footer" : false
+			}
+		});
+		if (jsonData.is_admin === 1) {
+			ssd_table.column(-1).visible(true);
+		}
+		// Apply the search
+		$('#filterCols th').each(function (idx){
+			var col = ssd_table.column(idx);
+			$('input', this).on( 'keyup change', function () {
+				col.search( this.value ).draw();
+			});
+		});
 	});
 
 	$.get('/data/memory', function(jsonData) {
-  	var memory_table = $('#memory_table').DataTable({
+		var memory_table = $('#memory_table').DataTable({
 		"data": jsonData.items,
 		"columns" : [
 			{"data" : "serial_num"},
@@ -97,28 +97,28 @@ $(document).ready(function() {
 			{"defaultContent": "<button class=\"scrap_btn\">Scrap</button>", "visible": false}
 		],
 		"scrollX" 	  : true,
-	  "paging" 		  : true,
+		"paging" 		  : true,
 		"pagingType"  : "simple_numbers",
 		"pageLength"  : 10,
-	  "fixedHeader" : {
-	    	 "header" : true,
-	    	 "footer" : false
-	    }
-	  });
-	  if (jsonData.is_admin === 1) {
-	  	memory_table.column(-1).visible(true);
-	  }
-	  // Apply the search
-	  $('#filterCols th').each(function (idx){
-	  	var col = memory_table.column(idx);
-	  	$('input', this).on( 'keyup change', function () {
-	  		col.search( this.value ).draw();
-	  	});
-	  });
+		"fixedHeader" : {
+				 "header" : true,
+				 "footer" : false
+			}
+		});
+		if (jsonData.is_admin === 1) {
+			memory_table.column(-1).visible(true);
+		}
+		// Apply the search
+		$('#filterCols th').each(function (idx){
+			var col = memory_table.column(idx);
+			$('input', this).on( 'keyup change', function () {
+				col.search( this.value ).draw();
+			});
+		});
 	});
 
-  $.get('/data/flash', function(jsonData) {
-  	var flash_table = $('#flash_table').DataTable({
+	$.get('/data/flash', function(jsonData) {
+		var flash_table = $('#flash_table').DataTable({
 		"data": jsonData.items,
 		"columns" : [
 			{"data" : "serial_num"},
@@ -128,24 +128,24 @@ $(document).ready(function() {
 			{"defaultContent": "<button class=\"scrap_btn\">Scrap</button>", "visible": false}
 		],
 		"scrollX" 	  : true,
-	  "paging" 		  : true,
+		"paging" 		  : true,
 		"pagingType"  : "simple_numbers",
 		"pageLength"  : 10,
-	  "fixedHeader" : {
-	    	 "header" : true,
-	    	 "footer" : false
-	    }
-	  });
-	  if (jsonData.is_admin === 1) {
-	  	flash_table.column(-1).visible(true);
-	  }
-	  // Apply the search
-	  $('#filterCols th').each(function (idx){
-	  	var col = flash_table.column(idx);
-	  	$('input', this).on( 'keyup change', function () {
-	  		col.search( this.value ).draw();
-	  	});
-	  });
+		"fixedHeader" : {
+				 "header" : true,
+				 "footer" : false
+			}
+		});
+		if (jsonData.is_admin === 1) {
+			flash_table.column(-1).visible(true);
+		}
+		// Apply the search
+		$('#filterCols th').each(function (idx){
+			var col = flash_table.column(idx);
+			$('input', this).on( 'keyup change', function () {
+				col.search( this.value ).draw();
+			});
+		});
 	});
 
 });
